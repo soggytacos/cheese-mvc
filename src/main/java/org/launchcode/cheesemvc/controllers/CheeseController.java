261e24cb2bbf705afd3822f13506b2ead53c5135
swish <swisher66@gmail.com>
2019-08-17 09:12:23 -0500

    Added the remove functionality to the application

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String, String> cheeses = new HashMap<>();
 
     // Request path: /cheese
     @RequestMapping(value = "")
@@ -26,18 +27,35 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAddCheeseform(Model model) {
+    public String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
         return "cheese/add";
 
     }
     //cheeseName needs to match the name value of the form attribute. Then the black box of Spring will look for it.
     @RequestMapping(value ="add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
+        cheeses.put(cheeseName, cheeseDescription);
 
         // Redirect to /cheese
         return "redirect:";
     }
 
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", this.cheeses);
+        model.addAttribute("title", "Remove Cheeses");
+
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String removeCheese(@RequestParam ArrayList<String> cheeses) {
+
+        for (String cheese : cheeses) {
+            this.cheeses.remove(cheese);
+        }
+         // redirect back to the index
+        return "redirect:";
+    }
 }
